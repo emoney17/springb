d94b29ca36df7c0a220ae9c0277600e7e2531c6f
emoney17 <emmanuel.million@tutanota.com>
2023-03-09 11:02:09 -0600

    DELETE request complete

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -31,5 +33,11 @@ public class StudentController
     @PostMapping()
     public void registerNewStudent(@RequestBody Student student) {
 	studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(
+	@PathVariable("studentId") Long studentId) {
+	studentService.deleteStudent(studentId);
     }
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -35,4 +35,12 @@ public class StudentService
 	// System.out.println(student);
     }
 
+    public void deleteStudent(Long studentId) {
+	boolean exists = studentRepository.existsById(studentId);
+	if (!exists) {
+	    throw new IllegalStateException("student with id " + studentId + " does not exist");
+	}
+	studentRepository.deleteById(studentId);
+    }
+
 }
